285b6712935b2b91aa2e1e14818369c528790fe9
Miko Chu <miko@reach52.com>
2024-04-16 03:11:36 +0800

    added user model

--- GoodApp/Models/User.cs
+++ GoodApp/Models/User.cs
@@ -2,5 +2,10 @@
 {
     public class User
     {
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public required string LastName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
     }
 }
